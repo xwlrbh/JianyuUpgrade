02a29a9056574519994b326131981d74ea603448
swuuws <swuuws@yeah.net>
2020-06-08 10:03:28 +0800

    no message

--- jianyu/extra/jianyu.php
+++ jianyu/extra/jianyu.php
@@ -7,7 +7,7 @@
  * Copyright: http://jianyuluntan.com All rights reserved.
  */
 return [
-    'version' => '1.7.3',
+    'version' => '1.7.6',
     'name' => '剑鱼论坛',
     'description' => 'Catfish(鲶鱼) CMS官方发布的轻论坛程序',
     'official' => 'jianyuluntan.com'
